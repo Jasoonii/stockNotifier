61f21de1f6fad481d568cd19b0affcf37caa1a57
Jasoonii <jason.ddang1@gmail.com>
2026-06-02 10:29:21 -0500

    Update stock lists

--- stocks.py
+++ stocks.py
@@ -1,5 +1,5 @@
 #List of stocks to track
-stockList = ["NVDA", "AMD", "GOOGL", "MSFT", "AMZN"]
+stockList = ["NVDA", "AMD", "GOOGL", "MSFT", "AMZN", "VOO", "SOXX"]
 
 #Notify if a stock drops 2% or more
 alertThreshold = -0.02
