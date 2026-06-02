e963da86a11e6863f547c29996179b612116fa2e
Jasoonii <jason.ddang1@gmail.com>
2026-06-02 00:38:11 -0500

    Setting alert to show 2 decimal points to percentage change

--- notify.py
+++ notify.py
@@ -1,7 +1,7 @@
 import requests
 
 def sendAlert(notifyTopic, ticker, percentChange, currentPrice):
-    message = f"{ticker} is down {abs(percentChange) * 100:.1f}% today (${currentPrice:.2f})"
+    message = f"{ticker} is down {abs(percentChange) * 100:.2f}% today (${currentPrice:.2f})"
 
     requests.post(
         f"https://ntfy.sh/{notifyTopic}",
